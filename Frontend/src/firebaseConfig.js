f2381ab3901fc0dd68104bb632805fc3df3e8663
Aran169 <muruganarul1693@gmail.com>
2025-04-22 20:59:26 +0530

    Render

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyD_yn3Gm9oaA-IxOSrzcQ_-p3sPY8PErhQ",
   authDomain: "city-monitor-f8ac1.firebaseapp.com",
   projectId: "city-monitor-f8ac1",
-  storageBucket: "city-monitor-f8ac1.appspot.com",  // ✅ Fixed here
+  storageBucket: "city-monitor-f8ac1.appspot.com",  
   messagingSenderId: "58442493270",
   appId: "1:58442493270:web:1a555ed2b7432d799bbaf7",
   measurementId: "G-FYWD85YQQ6"
