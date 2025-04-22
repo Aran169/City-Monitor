f502974f10f6db91648ac714c4b9437c85121866
Aran169 <muruganarul1693@gmail.com>
2025-04-22 21:12:41 +0530

    Render

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-require("dotenv").config();
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 
